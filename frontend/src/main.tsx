fb5ab1c8d21fc21df5c692a52d440436e3dc69db
Camila Tigre <camila.tigre@gupy.com.br>
2025-06-19 10:00:46 -0300

    chore: set TaskPage as root component in main entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { TaskPage } from '@/pages/task-page';
 import { TasksProvider } from '@/context/tasks/tasks-context';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <TasksProvider>
-      <App />
+      <TaskPage />
     </TasksProvider>
   </React.StrictMode>,
 );
